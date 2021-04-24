48b6422272733f7318420926afe9101e677457b0
ruderude <rude1979@gmail.com>
2021-04-25 04:06:49 +0900

    fff

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-
 var canvas = document.getElementById("reversi")
 var ctx = canvas.getContext("2d")
-　　
+var playBoard = [
+	[0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+]
+
 ctx.fillStyle = "black"
 
 for (let col = 0; col < 8; col++) {
@@ -10,7 +19,6 @@ for (let col = 0; col < 8; col++) {
     ctx.lineTo(40 * col, 320); // Draw a line to
     ctx.stroke();
 }
-// 横の線を引く
 for (let row = 0; row < 8; row++) {
     ctx.beginPath(); // Start a new path
     ctx.moveTo(0, 40 * row); // Move the pen to
@@ -18,51 +26,51 @@ for (let row = 0; row < 8; row++) {
     ctx.stroke();
 }
 
-canvas.addEventListener('click', clickfunc, true);
-
-function clickfunc (e) {
-	var x = y = 0;
-
-	var rect = e.target.getBoundingClientRect();
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
-
-    putStone(x, y, 'white')
-
+function canPutStoneCheck(x, y) {
+    return playBoard[y][x] === 0 ? true : false
 }
 
-function chengeXY(num) {
-    if (num >= 0 && num <= 40) {
-        return 20
-    } else if(num >= 41 && num <= 80) {
-        return 60
-    } else if(num>=81 && num<=120) {
-        return 100
-    } else if(num>=121 && num<=160) {
-        return 140
-    } else if(num>=161 && num<=200) {
-        return 180
-    } else if(num>=201 && num<=240) {
-        return 220
-    } else if(num>=241 && num<=280) {
-        return 260
-    } else if(num>=281 && num<=320) {
-        return 300
-    } else {
-        return 20
-    }
-}
-
-function putStone(x, y, color) {
-    const x_point = chengeXY(x)
-    const y_point = chengeXY(y)
-    ctx.fillStyle = color;
+function putStone(x, y, color){
+	ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(x_point, y_point, 16, 0, 2 * Math.PI);
+    ctx.arc(x, y, 16, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-putStone(140, 140, "white")
-putStone(180, 140, "black")
-putStone(140, 180, "black")
-putStone(180, 180, "white")
+// クリックした座標に石を置く
+function putStoneAt(event){
+	// HTMLの中にCANVASがあるので、CANVASのどの位置をクリックしたか
+ 	// 調整するために、CANVASとHTMLの位置関係から補正しています
+    var rect = event.target.getBoundingClientRect();
+    x = event.clientX - rect.left;
+    y = event.clientY - rect.top;
+    x = x - x % 40 + 20 // キリが良い箇所に配置されるようにx座標を補正
+    y = y - y % 40 + 20 // キリが良い箇所に配置されるようにy座標を補正
+    var posX = (x-20) / 40
+    var posY = (y-20) / 40
+    if (canPutStoneCheck(posX, posY)) {
+        putStone(x, y, "white")
+    } else {
+        console.log('そこに石は置けません！')
+        return false
+    }
+    
+    // どこに石を置いたか記憶する
+    playBoard[posY][posX] = 1 // 1:white 2:black とする
+    console.log(playBoard)
+}
+
+// 初期配置を行う関数
+function initialize(){
+    putStone(140, 140, "white")
+    putStone(180, 140, "black")
+    putStone(140, 180, "black")
+    putStone(180, 180, "white")
+    playBoard[3][3] = 1
+    playBoard[3][4] = 2
+    playBoard[4][3] = 2
+    playBoard[4][4] = 1
+}
+
+// 初期配置を実装
+initialize()
