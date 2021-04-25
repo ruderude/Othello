bfbd83fbaa8a960891635bd712f0d96397f0c34a
ruderude <rude1979@gmail.com>
2021-04-25 11:12:40 +0900

    クイックして石を置く。プレイヤーチェンジまで。

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var canvas = document.getElementById("reversi")
+var displayPlayer = document.getElementById("player")
+var log = document.getElementById("log")
 var ctx = canvas.getContext("2d")
 var playBoard = [
 	[0,0,0,0,0,0,0,0],
@@ -10,6 +12,9 @@ var playBoard = [
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
 ]
+
+var player = 1
+displayPlayer.innerText = "white"
 
 ctx.fillStyle = "black"
 
@@ -26,11 +31,27 @@ for (let row = 0; row < 8; row++) {
     ctx.stroke();
 }
 
+function changePlayer(){
+    if(player == 1){
+        player = 2
+        displayPlayer.innerText = "black"
+    }else{
+        player = 1
+        displayPlayer.innerText = "white"
+    }
+}
+
 function canPutStoneCheck(x, y) {
     return playBoard[y][x] === 0 ? true : false
 }
 
-function putStone(x, y, color){
+function putStone(x, y, player) {
+    let color = "white"
+    if(player == 1){
+        color = "white"
+    }else{
+        color = "black"
+    }
 	ctx.fillStyle = color;
     ctx.beginPath();
     ctx.arc(x, y, 16, 0, 2 * Math.PI);
@@ -49,23 +70,24 @@ function putStoneAt(event){
     var posX = (x-20) / 40
     var posY = (y-20) / 40
     if (canPutStoneCheck(posX, posY)) {
-        putStone(x, y, "white")
+        putStone(x, y, player)
     } else {
         console.log('そこに石は置けません！')
         return false
     }
     
     // どこに石を置いたか記憶する
-    playBoard[posY][posX] = 1 // 1:white 2:black とする
+    playBoard[posY][posX] = player // 1:white 2:black とする
+    changePlayer()
     console.log(playBoard)
 }
 
 // 初期配置を行う関数
 function initialize(){
-    putStone(140, 140, "white")
-    putStone(180, 140, "black")
-    putStone(140, 180, "black")
-    putStone(180, 180, "white")
+    putStone(140, 140, 1)
+    putStone(180, 140, 2)
+    putStone(140, 180, 2)
+    putStone(180, 180, 1)
     playBoard[3][3] = 1
     playBoard[3][4] = 2
     playBoard[4][3] = 2
